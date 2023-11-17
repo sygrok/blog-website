ca9bdfab1939d65577e046d7895c2234204bc34e
Berkay Anduv <sygrok@hotmail.com>
2023-11-17 07:10:52 +0300

    Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Link,
+  RouterProvider,
+  createBrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+import "./App.css";
 function App() {
-  const router = createBrowserRouter([{ path: "/", element }]);
   return (
     <>
-      <RouterProvider router={router} />
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/createpost">create post</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createpost" element={<CreatePost />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   );
 }
