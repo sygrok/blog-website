cf1f6e2a9ea5fb99b1822f71a50744442798f46a
Berkay Anduv <sygrok@hotmail.com>
2023-11-17 07:40:44 +0300

    Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,37 @@ import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
 import "./App.css";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
+
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
+  //logout
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
   return (
     <>
       <Router>
         <nav>
           <Link to="/">Home</Link>
           <Link to="/createpost">create post</Link>
-          <Link to="/login">Login</Link>
+          {!localStorage.isAuth ? (
+            <Link to="/login">Login</Link>
+          ) : (
+            <button onClick={signUserOut}>Logout</button>
+          )}
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/createpost" element={<CreatePost />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
         </Routes>
       </Router>
     </>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
+function Login({ setIsAuth }) {
+  let navigate = useNavigate();
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+
   return (
     <>
-      <div>Login</div>
+      <div className="login">Sign In With Google to Continue</div>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
     </>
   );
 }
